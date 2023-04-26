f2912308cd0767a6ba6222c00b55e3f6efa42853
Eliseo Casas Bossicovich <beliseocasas@gmail.com>
2023-04-26 18:31:15 -0300

    correccion de errores en JSON y async

--- ProductManager.js
+++ ProductManager.js
@@ -1,26 +1,37 @@
 const fs = require('fs');
-
+const path = './products.json'
 class ProductManager{
-    constructor(){
-        this.path = './products.json';
-    }
-
-    async addProduct(product){
+    #iva = 1.21;
+    #profit = 1.50;
+    // constructor(){
+    //     this.path = './products.json';
+    // }
+    
+    async createProduct(obj){
+        const {name, description, price, stock, thumbnail} = obj;
+        const product = {
+            id: this.#newId() + 1,
+            code: 'ver' + this.#newCode() + 1,
+            name,
+            description,
+            price: price * this.#iva * this.#profit,
+            stock,
+            thumbnail
+        }
         try {
-            const productsFile = await this.getProducts();
+            const productsFile = await this.getObjets();
             productsFile.push(product);
-            await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
+            await fs.promises.writeFile(path, JSON.stringify(productsFile));
         } catch (error) {
             console.log(error);
         }
     }
-
-    async getProducts(){
+    async getObjets(){
         try {
-            if (fs.existsSync(this.path)){
-                const products = await fs.promises.readFile(this.path, 'utf8');
-                const productsJS = JSON.parse(products);
-                return productsJS;
+            if (fs.existsSync(path)){
+                const objets = await fs.promises.readFile(path, 'utf8');
+                const objetsJS = JSON.parse(objets);
+                return objetsJS;
             } else {
                 return []
             }
@@ -28,29 +39,50 @@ class ProductManager{
             console.log(error);  
         }
     }
-}
-    // function newId(){
-    //     let itemId = 0;
-    //     this.products.map((product) => {
-    //         if(product.id > itemId) itemId = product.id;
-    //     });
-    //     return itemId;
-    // }id
 
-    // getProductById = (idProduct) =>{
-    //     const product = this.products.find((product) => product.id === idProduct);
-    //     if(product) {
-    //         console.log(product);
-    //     } else {
-    //         console.log('Product not found');
-    //     }
-    // }
+
+    async #newId(){
+        let itemId = 0;
+        const objetsList = await this.getObjets();
+        const product = objetsList.find((product) => {
+            if(product.id > itemId) itemId = product.id;
+        });
+        return itemId;
+    }id
+
+    async #newCode(){
+        let itemCode = 0;
+        const objetsList = await this.getObjets();
+        const product = objetsList.find((product) => {
+            if(product.code != itemCode) itemCode = product.code;
+        });
+        return itemCode;
+    }code
+
+    async getProductById (id){
+        const objetsList = await this.getObjets();
+        const product = objetsList.find((product) => product.id === id);
+        if(product) {
+            console.log(product);
+        } else {
+            console.log('Product not found');
+        }
+    }
+    async getProductByCode (code){
+        const objetsList = await this.getObjets();
+        const product = objetsList.find((product) => product.code === code);
+        if(product) {
+            console.log(product);
+        } else {
+            console.log('Product not found');
+        }
+    }
+}
+
 
 const productManager = new ProductManager();
-
 const product1 = {
     code: 'Man1',
-    // id: newId(),
     name:'Manzanas',
     description:'Manzana deliciosa tamaño 80',
     price:900 * 1.21 * 1.50,
@@ -59,7 +91,6 @@ const product1 = {
 }
 const product2 = {
     code: 'Ban1',
-    // id: newId(),
     name:'Bananas',
     description:'Banannas Ecuatorianas Grandes',
     price:480 * 1.21 * 1.50,
@@ -68,7 +99,6 @@ const product2 = {
 }
 const product3 = {
     code: 'Kiw1',
-    // id: newId(),
     name:'Kiwi',
     description:'Kiwis a punto',
     price:1800 * 1.21 * 1.50,
@@ -76,16 +106,15 @@ const product3 = {
     thumbnail:'www.kiwi.com'
 }
 
-const test = async() => {
-    const get = await productManager.getProducts();
-    console.log('primer consulta', get);
-    await productManager.addProduct(product1);
-    const get2 = await productManager.getProducts();
-    console.log('segunda consulta', get2);
-    await productManager.addProduct(product2);
-    const get3 = await productManager.getProducts();
-    console.log('tercer consulta', get3);
-    await productManager.addProduct(product3);
+async function test(){
+    // Agrego productos
+    await productManager.createProduct(product1);
+    await productManager.createProduct(product2);
+    await productManager.createProduct(product3);
+    const products = await productManager.getObjets();
+    console.log(products);
+    await productManager.getProductById(1)
+    await productManager.getProductByCode()
 }
 
 test()
